24d2daf8c6597d9e22a18512ef86a7eda500b116
Giacomo <130729552+La-Giacomo@users.noreply.github.com>
2023-05-04 16:52:28 -0700

    Snake game 1.0
    Idk

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,114 @@
 
 package com.mycompany.app;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class SnakeGame extends JFrame implements ActionListener {
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
+    private static final int UNIT_SIZE = 10;
+    private static final int GAME_UNITS = (WIDTH * HEIGHT) / UNIT_SIZE;
+    private static final int DELAY = 75;
+    private final int[] x = new int[GAME_UNITS];
+    private final int[] y = new int[GAME_UNITS];
+    private int bodyParts = 6;
+    private int applesEaten = 0;
+    private int appleX;
+    private int appleY;
+    private char direction = 'R';
+    private boolean running = false;
+    private Timer timer;
+    private Random random;
+
+    public SnakeGame() {
+        setTitle("Snake Game");
+        setSize(WIDTH, HEIGHT);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        random = new Random();
+        timer = new Timer(DELAY, this);
+        timer.start();
+
+        addKeyListener(new MyKeyAdapter());
+        startGame();
     }
-}
+
+    private void startGame() {
+        newApple();
+        running = true;
+    }
+
+    private void paint(Graphics g) {
+        // Draw the background
+        g.setColor(Color.black);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        // Draw the apple
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        // Draw the snake
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+            } else {
+                g.setColor(new Color(45, 180, 0));
+            }
+            g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+        }
+
+        // Draw the score
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.BOLD, 20));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Score: " + applesEaten, (WIDTH - metrics.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
+    }
+
+    private void move() {
+        // Move the body of the snake
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        // Move the head of the snake
+        switch (direction) {
+            case 'U':
+                y[0] -= UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] += UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] -= UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] += UNIT_SIZE;
+                break;
+        }
+    }
+
+    private void checkApple() {
+        // Check if the snake has eaten an apple
+        if (x[0] == appleX && y[0] == appleY) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
+    }
+
+    private void checkCollisions() {
+        // Check if the snake has collided with itself
+        for (int i = bodyParts; i > 0; i--) {
+            if (x[0] == x[i] && y[0] == y[i]) {
+                running = false;
+            }
+        }
+
+        // Check if the snake has collided with the walls
